9ea518eacfbaea1fabf9f77846bebe6071959191
Joe Fredette <jfredett@gmail.com>
2013-08-31 23:55:12 -0400

    rough out the idea

--- bin/iv.sh
+++ bin/iv.sh
@@ -1,1 +1,18 @@
 #!/usr/bin/env bash
+
+# join a server
+#   - Example: "iv irc.freenode.net"
+#   - creates a command called 'freenode' which accepts other commands
+#   - essentially opens a channel to the chan/nickserv status screen
+# from a server command, join a room:
+#   - creates a `ii` connection + a command for interacting w/ that room
+# from a room command:
+#   - say something
+#   - private message someone
+#   - leave
+# leaving a room
+#   - archives the files to a .tar.bz
+#   - puts them where you configure them to go
+#   - removes the room command and stuff
+# leaving a server
+#   - removes the server command and stuff
